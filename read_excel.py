a5b4debe4f62334f1bcb9b062f87b82a95dd186d
Andreas Jung <info@zopyx.com>
2020-06-03 18:51:37 +0200

    updated

--- read_excel.py
+++ read_excel.py
@@ -6,6 +6,10 @@ sheet=book.sheet_by_index(0)
 
 num_rows = sheet.nrows
 
+
+def is_question_number(s):
+    return re.match('^\d*\.\d$', s) is not None
+
 def is_question_level(s, level=1):
     if level == 1:
         return s.count(".") == 0
@@ -20,16 +24,19 @@ for row in range(1, num_rows):
     q_number = sheet.cell(row, 0).value
     q_text = sheet.cell(row, 1).value
     q_description= sheet.cell(row, 2).value
+    q_label = sheet.cell(row, 3).value
+
+    if not is_question_number(q_number):
+        continue
     print(q_number, q_text, q_description)
-    if is_question_level(q_number, 1):
-        questions[q_number] = dict(
+
+    first_number = q_number.split(".")[0]
+    if first_number not in questions:
+        questions[first_number] = dict(
                 question=q_text,
                 desription=q_description, 
                 labels=dict())
-        questions[q_number]['labels'][q_number] = 'xx'
-    elif is_question_level(q_number, 2):
-        q_number2 = q_number.split('.', 1)[0]
-        questions[q_number2]['labels'][q_number] = 'xx'
+    questions[first_number]['labels'][q_number] = q_label
 
 import pprint
 pprint.pprint(questions)
