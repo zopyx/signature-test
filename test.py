622f3780037e51b0ab9bf2f6b4cc2ecdf0a1d89d
Andreas Jung <info@zopyx.com>
2020-06-04 19:21:54 +0200

    updated

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
+import pytest
 from read_excel import SheetParser
+
 
 def test_working():
     parser = SheetParser()
-    assert not parser.errors 
+    assert not parser.errors
+
 
 def test_working_parser():
     parser = SheetParser()
     parser.parse("rechner.xlsx")
     assert not parser.errors
+
+def test_working_invalid_filename():
+    parser = SheetParser()
+    with pytest.raises(IOError):
+        parser.parse("nosuchfile.xlsx")
